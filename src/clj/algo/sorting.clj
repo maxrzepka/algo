717d16f4a5e2da5e7af2a4a1b366a0e4b6ab4559
Maximilien Rzepka <maximilien.rzepka@gmail.com>
2013-08-21 07:16:59 +0200

    insertion and merge sort

--- src/clj/algo/sorting.clj
+++ src/clj/algo/sorting.clj
@@ -1,6 +1,7 @@
 (ns algo.sorting)
-
-;; clojure version of chapter 3 in Algorithms Unlocked by Thomas H. Cormen (2013 MIT)
+;; # Sorting Unlocked
+;;
+;; Clojure version of chapter 3-4 from [Algorithms Unlocked]() by [Thomas H. Cormen](http://twitter.com/thcormen) (2013 MIT)
 
 (defn linear-search
   "Search x in n first elements of coll and returns index when first match found.
@@ -16,7 +17,11 @@ If n is not provided, n = size of coll.
                    coll))
              ))))
 
-;; TODO add pre-condition
+(defn middle
+  "Returns the midpoint between p and r"
+  [p r]
+  (+ p (int (Math/floor (/ (- r p) 2)))))
+
 (defn binary-search
   "Same as linear-search but quicker and coll must be sorted."
   ([x n coll] (binary-search x 0 n coll))
@@ -28,9 +33,10 @@ If n is not provided, n = size of coll.
                (> x a) (recur x p (dec q) coll)
                (< x a) (recur x (inc q) r coll))))))
 
-;; Implementation of the selection sort algorithm
+;; ## Selection Sort
+;;
 (defn indexed-min [coll]
-  (reduce (fn [[e i] [a k]] (if ( < e a) [e i] [a k]))
+  (reduce (fn [[e i] [a k]] (if (> e a) [a k] [e i]))
          (map vector coll (range))))
 
 (defn swap-step [[s c]]
@@ -44,8 +50,92 @@ If n is not provided, n = size of coll.
        (concat (subvec c 0 i)
                (subvec c (min (count c) (inc i)))))])))
 
-(defn selection-sort [n coll]
-  (first
-   (first
-    (filter (fn [[s c]] (empty? c))
-            (iterate swap-step [[] (subvec (vec coll) 0 n)])))))
+(defn selection-sort
+  ([coll] (selection-sort (count coll) coll))
+  ([n coll]
+     (first
+      (first
+       (filter (fn [[s c]] (empty? c))
+               (iterate swap-step [[] (subvec (vec coll) 0 n)]))))))
+
+;; ## Insertion Sorting
+;;
+(defn in-between? [x a b]
+  (cond (nil? a) (<= x b)
+        (nil? b) (<= a x)
+        :else (<= a x b)))
+
+(defn insert-by-order
+  "returns scoll with x inserted at the right place."
+  [x scoll]
+  (if (or (empty? scoll) (<= (last scoll) x))
+    ;; case when scoll is empty or x 
+    (conj (vec scoll) x)
+    (second
+     (reduce (fn [[t a] e]
+               (if t
+                 (let [insertion? (in-between? x (last a) e)
+                       a (if insertion? (conj a x) a)]
+                   [(not insertion?) (conj a e)])
+                 [t (conj a e)]))
+             [true []]  scoll))))
+
+(defn insertion-sort
+  ([coll] (loop [scoll [] coll coll]
+            (if (empty? coll)
+              scoll
+              (recur (insert-by-order (first coll) scoll) (rest coll)))))
+  ([n coll]
+     (concat (insertion-sort (take n coll)) (drop n coll))))
+
+(defn insertion-sort1
+  ([coll] (reduce (fn [a e] (insert-by-order e a)) 
+                  [] coll))  
+  ([n coll]
+     (concat (insertion-sort1 (take n coll)) (drop n coll))))
+
+;; ## Merge Sort
+;;
+;; inspired (partially copied) from [Alexei Sholik's gist](https://gist.github.com/alco/2135276)
+(defn merge-seqs
+  ([[a b]] (merge-seqs a b))
+  ([a b]
+     (loop [res [] a a b b]
+       (cond
+        (every? empty? [a b]) res
+        (empty? a) (recur (concat res b) a [])
+        (empty? b) (recur (concat res a) [] b)        
+        (< (first a) (first b)) (recur (conj res (first a)) (rest a) b)
+        :else (recur (conj res (first b)) a (rest b))))))
+
+(defn merge-sort
+  ([coll]
+     (cond (< (count coll) 2) coll
+           :else
+           (let [q (bit-shift-right (count coll)  1)]
+             (merge-seqs (map merge-sort (split-at q coll))))))
+  ([p r coll]
+     (concat (take p coll)
+             (merge-sort (subvec coll p r))
+             (drop r coll))))
+
+;; ## Quick Sort
+
+(defn quick-sort
+  ([coll] (quick-sort (count coll) coll))  
+  ([n coll]
+     coll))
+
+;; ## Counting Sort
+
+(defn counting-sort
+  ([coll] (counting-sort (count coll) coll))  
+  ([n coll]
+     coll))
+
+;; ## Radix Sort
+
+(defn radix-sort
+  ([coll] (radix-sort (count coll) coll))  
+  ([n coll]
+     coll))
--- test/algo/test/sorting.clj
+++ test/algo/test/sorting.clj
@@ -2,20 +2,72 @@
   (:use algo.sorting
         clojure.test))
 
+(def datasets
+  "set of unsorted list and its sorted one "
+  { [4 2 1 10] [1 2 4 10]
+    [2 3 4 5 -1] [-1 2 3 4 5]
+    [1 2 4 1] [1 1 2 4]
+    [1 2 4 7] [1 2 4 7]
+    [89 1 2 3 4 5] [1 2 3 4 5 89]
+    })
+
 (deftest linear-search-test
-  (is (= 3 (linear-search 8 [6 9 6 8]))
-      (= 2 (linear-search 9 4 [2 2 9 3 4 5 10]))))
+  (is (= 3 (linear-search 8 [6 9 6 8])))
+  (is (= 2 (linear-search 9 4 [2 2 9 3 4 5 10]))))
+
+(deftest middle-test1
+  (doseq [n (range 1 20)]
+    (is (= (bit-shift-right n 1) (middle 0 n)))))
+
+(deftest middle-test
+  (is (= 1 (middle 0 2) ))
+  (is (= 2 (middle 1 3) ))
+  (is (= 7 (middle 4 10)))
+  (is (= 3 (middle 0 6) ))
+  (is (= 3 (middle 0 7) )))
 
 (deftest binary-search-test
-  (is (= 3 (linear-search 8 4 [6 9 6 8]))
-      (= 2 (linear-search 9 4 [2 2 9 3 4 5 10]))))
+  (is (= 3 (linear-search 8 4 [6 9 6 8])))
+  (is (= 2 (linear-search 9 4 [2 2 9 3 4 5 10]))))
 
 (deftest indexed-min-test
-  (is (= [1 3] (indexed-min [2 3 4 1]))
-      (= [2 2] (indexed-min [4 9 2 2]))))
+  (is (= [1 3] (indexed-min [2 3 4 1])))
+  (is (= [2 2] (indexed-min [4 9 2 2]))))
 
 (deftest swap-step-test
   (is (= [[-5] [2 3 4]] (swap-step [[] [2 3 4 -5]]))))
 
 (deftest selection-sort-test
-  (is (= [1 2 3 4] (selection-sort 4 [2 3 4 1 7]))))
+  (is (= [1 2 3 4] (selection-sort 4 [2 3 4 1 7])))
+  (doseq [[unsorted sorted] (seq datasets)]
+    (is (= sorted (selection-sort unsorted))))
+  )
+
+(deftest insert-by-order-test
+  (is (= [1 2 3 4] (insert-by-order 3 [1 2 4])))
+  (is (= [1 2 4 10] (insert-by-order 10 [1 2 4])))
+  (is (= [1 1 4 10] (insert-by-order 1 [1 4 10])))
+  (is (= [1 4 4 4 10] (insert-by-order 4 [1 4 4 10]))))
+
+(deftest insertion-sort-test
+  (doseq [[unsorted sorted] (seq datasets)]
+    (is (= sorted (insertion-sort unsorted)))))
+
+(deftest insertion-sort1-test
+  (doseq [[unsorted sorted] (seq datasets)]
+    (is (= sorted (insertion-sort1 unsorted)))))
+
+(deftest merge-seqs-test
+  (is (= [1 2 3 4 7 10] (merge-seqs [1 4 7] [2 3 10]))))
+
+(deftest merge-sort-test
+  (doseq [[unsorted sorted] (seq datasets)]
+    (is (= sorted (merge-sort unsorted)))))
+
+#_(deftest counting-sort-test
+  (doseq [[unsorted sorted] (seq datasets)]
+    (is (= sorted (counting-sort unsorted)))))
+
+#_(deftest radix-sort-test
+  (doseq [[unsorted sorted] (seq datasets)]
+    (is (= sorted (radix-sort unsorted)))))
